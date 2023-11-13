b6d02bedfc479d81796d470356751300fb85078c
WebDevDB <db.dci.student@gmail.com>
2023-11-13 18:13:52 +0100

    Add comment With ChatGPT

--- src/components/API-GET/Search.js
+++ src/components/API-GET/Search.js
@@ -1,19 +1,29 @@
+// Importieren der benötigten Hooks von React
 import { useState, useEffect } from 'react';
 
+// Definition der Search-Funktion, die als Komponente dient
 function Search(props) {
+  // Verwendung des useState-Hooks, um den Zustand der Suchanfrage und der Ergebnisse zu verwalten
   const [query, setQuery] = useState(props.searchTerm);
   const [results, setResults] = useState([]);
 
+  // Verwendung des useEffect-Hooks, um die Suchanfrage auszuführen, wenn sich der Suchbegriff ändert
   useEffect(() => {
+    // Überprüfung, ob ein Suchbegriff vorhanden ist
     if (query) {
+      // Ausführung der Suchanfrage mit fetch
       fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`)
+        // Umwandlung der Antwort in JSON
         .then(response => response.json())
+        // Aktualisierung des Zustands der Ergebnisse mit den erhaltenen Daten
         .then(data => setResults(data.meals));
     }
-  }, [query]);
+  }, [query]); // Abhängigkeiten des useEffect-Hooks
 
+  // Rückgabe der Komponente, die die Suchergebnisse anzeigt
   return (
     <div>
+      {/* Überprüfung, ob Ergebnisse vorhanden sind, und dann deren Darstellung */}
       {results && results.map((meal) => (
         <div key={meal.idMeal}>
           <h2>{meal.strMeal}</h2>
@@ -25,4 +35,5 @@ function Search(props) {
   );
 }
 
+// Exportieren der Search-Komponente als Standardexport
 export default Search;
