2b00776d58a9d8c064c33208a868d9301348b1b8
Julia Erbis <iuliia.erbis@gmail.com>
2023-11-14 15:30:37 +0100

    Footer Form und Social media

--- src/components/reusable/Footer.jsx
+++ src/components/reusable/Footer.jsx
@@ -1,7 +1,55 @@
-
+import { useState } from "react";
+import "./Footer.css";
 
 export const Footer = () => {
+  const [submitted, setSubmitted] = useState(false);
+
+  const messageHandler = (event) => {
+    event.preventDefault();
+    setMessageInput(event.target.textfeld.value);
+  };
+
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="flex flex-wrap justify-center items-center bg-gray-900 py-10">
+      {submitted ? (
+        <p>Thanks, for your Feedback!</p>
+      ) : (
+        <form
+          onSubmit={messageHandler}
+          className="flex flex-col m-4 items-center"
+        >
+          <h2 className="text-white font-medium">Feedback</h2>
+          <textarea
+            id="message"
+            rows="5"
+            name="textfeld"
+            placeholder="Ihre Nachricht"
+            className="bg-gray-700 rounded-md"
+          ></textarea>
+          <div className=" my-3 ">
+            <button className="btn text-gray bg-green-600 rounded-md font-medium">
+              Absenden
+            </button>
+          </div>
+        </form>
+      )}
+
+      <div className="text-white p-5 flex flex-col social-media">
+        <h2 className="text-center">Social Media</h2>
+        <div className="text-green-600 icons ">
+          <i className="fa-brands fa-twitter"></i>
+
+          <i className="fa-brands fa-square-facebook"></i>
+
+          <i className="fa-brands fa-square-instagram"></i>
+
+          <i className="fa-brands fa-pinterest"></i>
+        </div>
+      </div>
+
+      <div className="w-full text-xs pt-3 text-center text-white ">
+        © 2023 Copywrite - Lecker-Schmecker
+      </div>
+    </footer>
+  );
+};
