5bdaf727faaecf0fb4928aecea5cc4700a932e5b
KassandraJK <jenskoeniger196@googlemail.com>
2023-11-14 15:29:52 +0100

    Search, Navbar und App.jsx angepasst.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
 import './App.css'
 import Navbar from './components/reusable/Navbar'
-import { Search } from './components/reusable/Search'
 import { Footer } from './components/reusable/Footer'
 import { Outlet } from 'react-router-dom'
-import { Meals } from './components/Meals'
 
 
 function App() {
@@ -12,8 +10,6 @@ function App() {
   return (
     <>
     <Navbar /> 
-    <Search />
-    {/* <Meals /> */}
     <Outlet />
     <Footer /> 
     </>
