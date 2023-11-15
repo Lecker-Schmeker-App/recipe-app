72e9413f10828000998dfd44239c9832a657ead4
KassandraJK <jenskoeniger196@googlemail.com>
2023-11-15 16:06:56 +0100

    Home Screen fertiggestellt.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,8 +2,8 @@
 
 export const Home = () => {
   return (
-    <div>
-      <div className="flex justify-center align-middle items-center p-5 text-5xl">
+    <div className="bg-blue-900">
+      <div className="flex text-slate-200 justify-center align-middle items-center p-5 text-5xl font-serif">
         <h1>Willkommen bei Lecker-Schmecker</h1>
       </div>
       <div className="flex justify-center align-middle items-center p-5">
