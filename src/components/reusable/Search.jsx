00bfb93e5536f40260b6ed89c28b7a5eca480d47
Julia Erbis <iuliia.erbis@gmail.com>
2023-11-15 19:01:40 +0100

    SearchBar erledigt

--- src/components/reusable/Search.jsx
+++ src/components/reusable/Search.jsx
@@ -1,17 +1,37 @@
-
+import { useContext, useState } from "react";
+import { MealsContext } from "../../contexts/MealsContext";
 
 export const Search = () => {
-  return (
-    
-<form className="flex items-center">   
-    <label for="simple-search" className="sr-only">Search</label>
-    <div className="relative w-full">
-        <input type="text" id="simple-search" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Recipes" required/>
-    </div>
-    <button type="submit" className="p-2.5 ms-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-      <i class="fa-solid fa-magnifying-glass"></i>
-    </button>
-</form>
+  const [searchInput, setSearchInput] = useState("");
+  const { setSearchTerm } = useContext(MealsContext);
 
-  )
-}
+  const searchHandler = (event) => {
+    setSearchInput(event.target.value);
+  };
+
+  const submittHandler = (event) => {
+    event.preventDefault();
+    setSearchTerm(searchInput);
+    setSearchInput("");
+  };
+
+  return (
+    <form className="flex items-center" onSubmit={submittHandler}>
+      <div className="relative w-full">
+        <input
+          type="text"
+          id="simple-search"
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          value={searchInput}
+          onChange={searchHandler}
+        />
+      </div>
+      <button
+        type="submit"
+        className="flex items-center ms-2 text-sm w-20 h-10 text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+      >
+        <i className="fa-solid fa-magnifying-glass"></i>
+      </button>
+    </form>
+  );
+};
