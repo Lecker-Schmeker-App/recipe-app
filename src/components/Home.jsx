f569e2546bdd926f909d5c4ada3c64f5cdba56ad
KassandraJK <jenskoeniger196@googlemail.com>
2023-11-15 11:16:59 +0100

    Home Screen erstellt.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,12 @@
 export const Home = () => {
   return (
     <div>
-        <h1>Hallo</h1>
+      <div className="flex justify-center align-middle items-center p-5 text-5xl">
+        <h1>Willkommen bei Lecker-Schmecker</h1>
+      </div>
+      <div className="flex justify-center align-middle items-center p-5">
+        <img src="./pictures/a-table-full-of-goodness.jpg" alt="Tisch mit verschiedenen Speisen."></img>
+      </div>
     </div>
   )
 }
