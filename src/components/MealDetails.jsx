40533c31d281097abe7c5e2e2af915c99e7e2ae6
Julia Erbis <iuliia.erbis@gmail.com>
2023-11-15 21:45:02 +0100

    MealsDetails hinzugefügt und vervollständigt

--- src/components/MealDetails.jsx
+++ src/components/MealDetails.jsx
@@ -1,5 +1,5 @@
 import { useParams } from "react-router-dom";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { MealsContext } from "../contexts/MealsContext";
 
 export const MealDetails = () => {
@@ -7,5 +7,24 @@ export const MealDetails = () => {
   const { meals } = useContext(MealsContext);
 
   const currentMeal = meals.meals.find((meal) => meal.idMeal === id);
-  return <div>{currentMeal.strMeal}</div>;
+
+  return (
+    <article className="cover">
+      <div className="modal-container">
+        <img src={currentMeal.strMealThumb} className="img-modal" />
+      </div>
+      <div className="">
+        <h4>{currentMeal.strMeal} </h4>
+        <p>Cooking Instructions</p>
+        <p>{currentMeal.strInstructions} </p>
+        <a
+          href={currentMeal.strSource}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Original Source
+        </a>
+      </div>
+    </article>
+  );
 };
